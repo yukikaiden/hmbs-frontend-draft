039cbf54d6c376910a9c8ff272499097c464be35
Kang Te-Oh <kangteoh19@gmail.com>
2025-07-17 15:18:11 +0800

    Modified Files

--- borrowing-system/src/App.jsx
+++ borrowing-system/src/App.jsx
@@ -3,24 +3,31 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import React from 'react';
-import LoginPage from './pages/Log-in';  // Adjust path as needed
+import AboutPage from './pages/AboutPage';
+import BorrowRequestForm from "./pages/BorrowRequestForm";
+import CartPage from "./pages/CartPage";
+import EquipmentsPage from "./pages/EquipmentPage";
 import Homepage from './pages/Homepage';
+import LoginPage from './pages/Log-in';  
+
 import RequestAdminPage from "./pages/RequestAdminPage";
 import RequestDetailsAdmin from "./pages/RequestDetailsAdmin";
-import Sidebar from "./components/Sidebar";
 import RequestApprovedAdmin from "./pages/RequestApprovedAdmin";
-import EquipmentsPage from "./pages/EquipmentPage";
+import CRUDInventoryPage from "./pages/CRUDInventoryPage";
+import CRUDUserPageAdmin from "./pages/CRUDUserPageAdmin";
+import AddNewItemAdmin from './pages/AddNewItemAdmin';
+
 import RequestInstructorPage from "./pages/RequestInstructorPage";
 import RequestDetailsInstructor from "./pages/RequestDetailsInstructor";
+
 import RequestProgHeadPage from "./pages/RequestProgHeadPage";
 import RequestDetailsProgHead from "./pages/RequestDetailsProgHead";
-import BorrowRequestForm from "./pages/BorrowRequestForm";
-import RequestSubmittedModal from "./components/RequestSubmittedModal";
+
 
 function App() {
     return (
         <div>
-            <BorrowRequestForm />
+            < RequestDetailsAdmin/>
         </div>
     );
 }
--- borrowing-system/src/pages/AboutPage.jsx
+++ borrowing-system/src/pages/AboutPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import aboutHeaderBg from '../assets/about-header-bg.png';
 
 function AboutPage() {
